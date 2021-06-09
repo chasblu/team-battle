340fb55ff9cbdca84f31d367058da0f0cfc39ff1
chasblu <c.prit@ymail.com>
2021-06-09 13:17:27 -0400

    Working on functions

---
+++ script.js
@@ -0,0 +1,49 @@
+// Player vs comp 
+// MVP 
+// On game load the game board appears with an instructions modal and the character selector screen
+// Player chooses his character by clicking on the character on the screen.
+// this will be a image inside of a div
+// when he clicks on the character the character image moves to the player stage. Player always chooses first
+// the character that has been selected is removed from the options.
+// Next it is the computers turn to select.  The computer picks randomly from the remaining options. 
+// The computer selection is removed from the character list and is placed on the computers stage.
+// Once both teams are done being selected all the character selction images are removed from the screen.
+// the game moves into the battle stage
+// The player chooses its attack first.  The player clicks on the computers character image to attack. 
+// The player does this for each character he has on his stage.
+// the computer chooses randomly who it attacks and the the fight scene plays out(for MVP just dialogue)
+// The damage dealt/health lost is equal to the attack power of the character(will convert this to an attack power range if time allows)
+// when a characters health reaches 0 it is eliminated and cannot attack anymore
+// eliminated character images are removed from the stages when they are eliminated
+// Once an entire team is eliminated it loses.  Screen reads who won.
+// button to reset the board
+
+// GLOBAL VARIABLE
+PLAYER_TEAM_LENGTH = 1
+COMP_TEAM_LENGTH = 1
+
+// helper function
+// const randomNumber = () => 
+
+class Character {
+    constructor(name, health, attackPower, accuracy)
+    this.name = name
+    this.health = health,
+    this.attackPower = attackPower
+    this.accuracy = 
+
+}
+
+const superman = new Character ("Superman", 100, 10)
+}
+
+const goku = new Character('Goku', 100, 15)
+
+
+// const playGame = () => {
+//     while
+// }
+
+
+
+ 
