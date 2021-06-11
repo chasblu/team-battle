20e9668e5b64f1c5282f0d0deaaf2fbf2416ad1c
chasblu <c.prit@ymail.com>
2021-06-11 12:35:25 -0400

    erased psuedocode

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-// Player vs comp 
-// MVP 
-// On game load the game board appears with an instructions modal and the character selector screen
-// Player chooses his character by clicking on the character on the screen.
-// this will be a image inside of a div
-// when he clicks on the character the character image moves to the player stage. Player always chooses first
-// the character that has been selected is removed from the options.
-// Next it is the computers turn to select.  The computer picks randomly from the remaining options. 
-// The computer selection is removed from the character list and is placed on the computers stage.
-// Once both teams are done being selected all the character selection images are removed from the screen.
-// the game moves into the battle stage
-// Each character only attacks once per turn
-// The player chooses its attack first.  The player clicks on the computers character image to attack. 
-// The player does this for each character he has on his stage.
-// the computer chooses randomly who it attacks for each character on the computer stage and the the fight scene plays out(for MVP just dialogue)
-// The damage dealt/health lost, is equal to the attack power of the character(will convert this to an attack power range if time allows)
-// when a characters health reaches 0 it is eliminated and cannot attack anymore
-// eliminated character images are removed from the stages when they are eliminated
-// Once an entire team is eliminated it loses.  Screen reads who won.
-// button to reset the board
+
 
 // DOM VARIABLES
 const resultsDisplay = document.querySelector('.results')
